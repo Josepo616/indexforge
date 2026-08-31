f739acd8d17546fd71b2ffbd1f728043b9bfbae1
José Álvarez <josemejia61976751@gmail.com>
2026-08-31 12:27:12 -0600

    chore: initial commit / scaffold macOS app

---
+++ Rastro/RastroApp.swift
@@ -0,0 +1,17 @@
+//
+//  RastroApp.swift
+//  Rastro
+//
+//  Created by JoseAlvarez on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct RastroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
